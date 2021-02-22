5f8892ce26aa11d07509d9c922b858025ea0449b
cubarabara <cubarabara@gmail.com>
2021-02-22 17:12:36 +0700

    frist commit

---
+++ Arduino/Pet_Feeder.ino
@@ -0,0 +1,60 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Servo.h>
+
+Servo servo;
+int m;
+int gateStart = 0;
+int gateEnd = 95;
+int gateOpenTime = 200;
+
+char auth[] = "gyFYeygYpetXZhg9MGUFgTB-GK_KCPjO";
+char ssid[] = "Rafi";
+char pass[] = "87654321";
+
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  servo.attach(15); // NodeMcu D8 Pin 
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
+BLYNK_WRITE(V3)
+{
+  servo.write(gateEnd);
+  delay(gateOpenTime);
+  servo.write(gateStart);
+}
+
+BLYNK_WRITE(V0)
+{
+  m = param.asInt();
+}
+
+
+BLYNK_WRITE(V1)
+{
+  for(int i=0;i<=m;i++)
+  {
+    servo.write(gateEnd);
+    delay(gateOpenTime);
+    servo.write(gateStart);
+  }
+}
+
+BLYNK_WRITE(V2) 
+{
+  int pinData = param.asInt();
+  if(pinData == 1)
+  {
+    servo.write(gateEnd);
+    delay(gateOpenTime);
+    servo.write(gateStart); 
+  }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Internet Of Things IOT Pet-Feeder BLynk
+
+## BLYNK
+
+![](Blynk.jpg)
+
+![](Blynk_Step_V_Pin_V0.jpg)
+
+![](Blynk_Styled_Button_Pin_V1.jpg)
+
+![](Bylnk_Timer_Pin_V2.jpg)
+
+![](Blynk_Styled_Button_Manual_Pin V3.jpg)
+
+## Application BLYNK Description 
+
+* Widget Step V                : Pin Virtual 0
+* Widget Styled                : Button Pin Virtual 1
+* Widget Timer                 : Pin Virtual 2
+* Widget Styled Button ' Manul : Pin Virtual 3
+
+## CIRCUIT DIAGRAM
